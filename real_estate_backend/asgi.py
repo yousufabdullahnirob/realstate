e9c29c68ffc188bb0cabc36bd44001f32142e22d
yousufabdullahnirob <yousufabdullahn@gmail.com>
2026-02-15 20:50:51 +0600

    Remove docstring from asgi.py
    Removed docstring comments from asgi.py.

--- real_estate_backend/asgi.py
+++ real_estate_backend/asgi.py
@@ -1,11 +1,4 @@
-"""
-ASGI config for real_estate_backend project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
 
 import os
 
