41d52ea2119c5c173556a8874535c5c6dbb70fcc
yousufabdullahnirob <yousufabdullahn@gmail.com>
2026-02-16 19:55:34 +0600

    Implement Login, Property Listing, and Backend Auth APIs

---
+++ core/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        return user
--- real_estate_backend/urls.py
+++ real_estate_backend/urls.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),
+    path('', views.apartment_list_view, name='apartment_list'),
+    path('api/register/', views.RegisterView.as_view(), name='api_register'),
+    path('api/login/', views.CustomLoginView.as_view(), name='api_login'),
 ]
