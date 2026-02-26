7fda05b57c1939d5f01e7916f432f5db1bd85ee4
ManhaMahbub <manhamahbubpushpita@gmail.com>
2026-02-26 14:52:03 +0600

    Connected landing.html to React app - Added Landing and Projects components with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import Landing from "./Landing";
+import Projects from "./Projects";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
