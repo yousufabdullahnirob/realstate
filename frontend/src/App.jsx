5d5d8d16365daccc6b44a263b296a67e6487b3f1
ManhaMahbub <manhamahbubpushpita@gmail.com>
2026-02-26 14:58:17 +0600

    Added ProjectDetails component - all pages connected to React router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Landing from "./Landing";
 import Projects from "./Projects";
+import ProjectDetails from "./ProjectDetails";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/project-details" element={<ProjectDetails />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
